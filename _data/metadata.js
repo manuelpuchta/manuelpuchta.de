23db51bcd9948e13c1a7c284b199ebc011bb4710
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2026-07-05 09:54:22 +0200

    Remove mail from metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -5,7 +5,6 @@ export default {
 	description: "Manuel Puchta is a web developer living in Hamburg",
 	author: {
 		name: "Manuel Puchta",
-		email: "mvstuckrad@pm.me",
 		url: "https://manuelpuchta.de/about/",
 	},
 };
